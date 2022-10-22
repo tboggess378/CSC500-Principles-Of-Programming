4a024247657b1bd0394c585098b66f414f74f9de
tboggess <tboggess378@gmail.com>
2022-10-22 11:05:30 -0400

    completed module 2 critical thinking assignment

--- CSC500_PrinciplesOfProgramming/Critical Thinking Assignment Module 2/mod_2_retail_total.py
+++ CSC500_PrinciplesOfProgramming/Critical Thinking Assignment Module 2/mod_2_retail_total.py
@@ -1,27 +1,32 @@
-try:
-    # Gets input from user for cost of each item
-    item_price = float(input('Enter price for the first item: \n'))
-    item_price2 = float(input('Enter price for the second item: \n'))
-    item_price3 = float(input('Enter price for the third item: \n'))
-    item_price4 = float(input('Enter price for the fourth item: \n'))
-    item_price5 = float(input('Enter price for the fifth item: \n'))
+def main():
+    try:
+        # Gets input from user for cost of each item
+        item_price = float(input('Enter price for the first item: \n'))
+        item_price2 = float(input('Enter price for the second item: \n'))
+        item_price3 = float(input('Enter price for the third item: \n'))
+        item_price4 = float(input('Enter price for the fourth item: \n'))
+        item_price5 = float(input('Enter price for the fifth item: \n'))
 
-    # Calculates subtotal of items and total with sales tax of 7%
-    sub_total = item_price + item_price2 + item_price3 + item_price4 + item_price5
-    sales_tax = 7
-    total = sub_total * (sales_tax / 100)
+        # Calculates subtotal of items and total with sales tax of 7%
+        sub_total = item_price + item_price2 + item_price3 + item_price4 + item_price5
+        sales_tax = 7
+        total = sub_total + sub_total * (sales_tax / 100)
 
-    # Output statements to show item prices, subtotal, sales tax, and total
-    print('RECEIPT')
-    print(f'Item 1 price: {item_price:.2f}')
-    print(f'Item 2 price: {item_price2:.2f}')
-    print(f'Item 3 price: {item_price3:.2f}')
-    print(f'Item 4 price: {item_price4:.2f}')
-    print(f'Item 5 price: {item_price5:.2f}')
-    print(f'Subtotal: {sub_total:.2f}')
-    print(f'Sales tax: {sales_tax}%')
-    print(f'Total: {total:.2f}')
+        # Output statements to show item prices, subtotal, sales tax, and total
+        print('RECEIPT')
+        print(f'Item 1 price: {item_price:.2f}')
+        print(f'Item 2 price: {item_price2:.2f}')
+        print(f'Item 3 price: {item_price3:.2f}')
+        print(f'Item 4 price: {item_price4:.2f}')
+        print(f'Item 5 price: {item_price5:.2f}')
+        print(f'Subtotal: {sub_total:.2f}')
+        print(f'Sales tax {sales_tax}%: {(sub_total * (sales_tax / 100.0)):.2f}')
+        print(f'Total: {total:.2f}')
 
-# Displays error message when user enters any non number
-except ValueError:
-    print('Error. Invalid Entry.')
+    # Displays error message when user enters any non number
+    except ValueError:
+        print('Error. Invalid Entry.')
+
+
+if __name__ == "__main__":
+    main()
