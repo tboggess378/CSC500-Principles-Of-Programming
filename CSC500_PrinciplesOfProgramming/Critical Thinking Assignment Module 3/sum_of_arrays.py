3f8e0056d6721d9bd8fff7e46a11785c54985aa5
tboggess <tboggess378@gmail.com>
2022-10-29 13:40:30 -0400

    just updates

--- CSC500_PrinciplesOfProgramming/Critical Thinking Assignment Module 3/sum_of_arrays.py
+++ CSC500_PrinciplesOfProgramming/Critical Thinking Assignment Module 3/sum_of_arrays.py
@@ -10,12 +10,15 @@ def main():
     arr2 = np.random.randint(1, target_number, size=5)
     arr3 = np.random.randint(1, target_number, size=5)
 
-    # Iterates through 3 arrays to find sums that will equate to target number
-    for i in range(arr1.size):
-        for j in range(arr2.size):
-            for k in range(arr3.size):
-                if (arr1[i] + arr2[j] + arr3[k]) == target_number:
-                    print(f'{arr1[i]} + {arr2[j]} + {arr3[k]} = {target_number}')
+    try:
+        # Iterates through 3 arrays to find sums that will equate to target number
+        for i in range(arr1.size):
+            for j in range(arr2.size):
+                for k in range(arr3.size):
+                    if (arr1[i] + arr2[j] + arr3[k]) == target_number:
+                        print(f'{arr1[i]} + {arr2[j]} + {arr3[k]} = {target_number}')
+    except ValueError:
+        raise 'Invalid character entered.'
 
 
 if __name__ == '__main__':
