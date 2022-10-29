5aa676ffba4c478d81ffeb71416620a5a36a3224
tboggess <tboggess378@gmail.com>
2022-10-29 13:44:31 -0400

    just updates

--- CSC500_PrinciplesOfProgramming/Critical Thinking Assignment Module 3/sum_of_arrays.py
+++ CSC500_PrinciplesOfProgramming/Critical Thinking Assignment Module 3/sum_of_arrays.py
@@ -12,11 +12,17 @@ def main():
 
     try:
         # Iterates through 3 arrays to find sums that will equate to target number
+        found_value = False
         for i in range(arr1.size):
             for j in range(arr2.size):
                 for k in range(arr3.size):
                     if (arr1[i] + arr2[j] + arr3[k]) == target_number:
                         print(f'{arr1[i]} + {arr2[j]} + {arr3[k]} = {target_number}')
+                        found_value = True
+
+        if not found_value:
+            print('No values found.')
+
     except ValueError:
         raise 'Invalid character entered.'
 
