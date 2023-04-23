6bf2c58d65f30e22370c60212bab0a30c95b0bff
tboggess <tboggess378@gmail.com>
2023-04-23 17:04:20 -0400

    module 1 simple program to display directory tree

---
+++ CSC505_PrinciplesOfSoftwareDevelopment/Module 1/Job_web_scraper.py
@@ -0,0 +1,48 @@
+import os
+import time
+
+
+def display_times(in_directory):
+    # Initialized variables to create string tree
+    num_tabs = 0
+    str_times_tree = f""
+    str_tab = ""
+
+    # Traverse through all the directories and files starting at the given directory
+    for (root, dirs, files) in os.walk(in_directory):
+
+        # Add the root directory to the string
+        if num_tabs > 0:
+            str_times_tree = str_times_tree + f"|\n{str_tab} Directory \"{root}\" created/modified: {time.ctime(os.path.getctime(root))}\n"
+        else:
+            str_times_tree = str_times_tree + f"{str_tab}Root \"{root}\" created/modified: {time.ctime(os.path.getctime(root))}\n"
+
+        # Each of the following is another subdirectory or file under the root directory
+        num_tabs = num_tabs + 1
+        str_tab = f"\\{'-' * num_tabs}>"
+
+        # Add each sub-directory to the root directory string
+        for directory in dirs:
+            d_time = os.path.getctime(os.path.join(in_directory, root, directory))
+            str_times_tree = str_times_tree + f"|\n{str_tab} Directory \"{directory}\" created/modified: {time.ctime(d_time)}\n"
+
+        # Add each file in the root directory string
+        for file in files:
+            f_time = os.path.getctime(os.path.join(in_directory, root, file))
+            str_times_tree = str_times_tree + f"|\n{str_tab} File \"{file}\" created/modified: {time.ctime(f_time)}\n"
+
+    return str_times_tree
+
+
+def main():
+    # Allow for an input string to print the directory tree
+    times_str = display_times(input('Enter the directory to print the tree first: '))
+    print(f"{times_str}")
+
+    # Save the string to an external file
+    with open('./Sample1.txt', 'w+') as f:
+        f.write(times_str)
+
+
+if __name__ == '__main__':
+    main()
